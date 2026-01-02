88fd863e32040a7ad63f36f56ef0d345e378eeca
Sudharshanan <sudharshanant@gmail.com>
2026-01-02 13:22:15 +0530

    Fix percentage calculation in recommend function to remove unnecessary rounding

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def recommend():
         results = [(career, 0.0) for career, _ in raw_scores]
     else:
         for career, score in raw_scores:
-            percent = round((score / total_score) * 100, 0)
+            percent = round((score / total_score) * 100)
             results.append((career, percent))
 
     results.sort(key=lambda x: x[1], reverse=True)
